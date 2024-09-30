c21114e2080caa0f50a46e4a028d6c992d8bf7ac
Pulkit Khandelwal <kpulkit95@gmail.com>
2024-09-29 21:35:33 -0400

    Update my_functions.py

--- misc_scripts/my_functions.py
+++ misc_scripts/my_functions.py
@@ -49,7 +49,20 @@ def save_nifti(image, filepath_name, img_obj):
     img = nib.Nifti1Image(image, img_obj.affine, header=img_obj.header)
     nib.save(img, filepath_name)
 
+def blockshaped(arr, nrows, ncols):
+    """
+    Return an array of shape (n, nrows, ncols) where
+    n * nrows * ncols = arr.size
 
+    If arr is a 2D array, the returned array should look like n subblocks with
+    each subblock preserving the "physical" layout of arr.
+    """
+    h, w = arr.shape
+    assert h % nrows == 0, "{} rows is not evenly divisble by {}".format(h, nrows)
+    assert w % ncols == 0, "{} cols is not evenly divisble by {}".format(w, ncols)
+    return (arr.reshape(h//nrows, nrows, -1, ncols)
+               .swapaxes(1,2)
+               .reshape(-1, nrows, ncols))
 
 
 
